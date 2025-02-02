5accf1646bb866da7291aabcff47a072b46c0942
imminsung <htk4064@flexenm.com>
2025-02-03 00:06:08 +0900

    Initial Commit

---
+++ SwiftBasic/SwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBasicApp.swift
+//  SwiftBasic
+//
+//  Created by 김민성 on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
